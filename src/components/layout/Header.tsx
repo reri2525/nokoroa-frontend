1d5d989e98a6598a66c6ce8a269a0d61f996ce6c
sugita <yuki.sugita@arteryex.biz>
2025-05-01 23:55:18 +0900

    first commit

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,38 @@
+'use client';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <AppBar
+      position="static"
+      elevation={0}
+      sx={{
+        height: '16vh', // 高さを 16 vh に固定
+        justifyContent: 'center', // Toolbar を縦方向中央寄せ
+        bgColor: 'background.paper', // 好みで
+      }}
+    >
+      <Toolbar disableGutters sx={{ px: 3 /* 左右の余白 */ }}>
+        {/* 左側：タイトル */}
+        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+          TITLE
+        </Typography>
+
+        {/* 右側：ボタンたち */}
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <Button variant="outlined" color="inherit">
+            ログイン
+          </Button>
+          <Button variant="contained" color="secondary">
+            新規登録
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
